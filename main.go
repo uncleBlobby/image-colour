cb8225e169a34e0c922b0095ab0e4bba8477e047
Dustin Christianson <dc.christianson@gmail.com>
2025-11-02 13:37:28 -0600

    refactor complete (?)

--- main.go
+++ main.go
@@ -25,16 +25,6 @@ const WALLPAPER_FILEPATH_TEMPLATE = "{{TEMPLATE_WALLPAPER_FILEPATH}}"
 func main() {
 	fmt.Println("hello, image-colors!")
 
-	// dir, err := os.ReadDir(WALLPAPER_DIR)
-	// if err != nil {
-	// 	log.Printf("Error reading image directory: %s", err)
-	// 	os.Exit(1)
-	// }
-
-	// randInd := rand.Intn(len(dir))
-
-	// fileName := fmt.Sprintf("%s", WALLPAPER_DIR+dir[randInd].Name())
-
 	fileName, err := getRandomWallpaperPath()
 	if err != nil {
 		log.Printf("[FAILED]: %s", err)
@@ -51,95 +41,52 @@ func main() {
 
 	sortedColors := sortColorMap(colorMap)
 
-	// reader, err := os.Open(fileName)
-	// if err != nil {
-	// 	log.Printf("Error reading image file: %s", err)
-	// 	os.Exit(1)
-	// }
-
-	// m, _, err := image.Decode(reader)
-	// if err != nil {
-	// 	log.Printf("Error decoding image file: %s", err)
-	// 	os.Exit(1)
-	// }
-
-	// bounds := m.Bounds()
-
-	// colorMap := map[color.Color]int{}
-
-	// for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-	// 	for x := bounds.Min.X; x < bounds.Max.X; x++ {
-	// 		colorMap[m.At(x, y)] += 1
-	// 	}
-	// }
-
-	// colors := make([]color.Color, 0, len(colorMap))
-	// for color := range colorMap {
-	// 	colors = append(colors, color)
-	// }
-	// sort.Slice(colors, func(i, j int) bool { return colorMap[colors[i]] > colorMap[colors[j]] })
-
-	// outColors := []color.Color{}
-
-	// for rank, col := range sortedColors {
-	// 	if rank < 1000 {
-	// 		fmt.Printf("%v - %d\n", col, colorMap[col])
-	// 		outColors = append(outColors, col)
-	// 	}
-	// }
-
 	top5Colours := getTopXColors(sortedColors, 5)
 
 	img := createPalettePNG(top5Colours)
-	// f2, err := os.Create("/home/dustin/.config/wallpaper/cols.wp")
-	// if err != nil {
-	// 	log.Printf("Error writing cols.wp: %s", err)
-	// }
 
 	err = writePalettePNGFile(img)
 	if err != nil {
 		log.Printf("Error writing palette PNG file: %s", err)
 	}
 
-	f3, err := os.Create(CONFIG_FILE_GENERATED)
-	if err != nil {
-		log.Printf("Error generating config file: %s", err)
-	}
+	// f3, err := os.Create(CONFIG_FILE_GENERATED)
+	// if err != nil {
+	// 	log.Printf("Error generating config file: %s", err)
+	// }
 
 	colorStrings := getColorStringsForConfigTemplate(top5Colours)
 
-	// for ind, c := range outColors {
-	// 	if ind < 5 {
-	// 		fmt.Println(c)
-	// 		// fmt.Printf("%f\n", c)
-	// 		v1, v2, v3, v4 := c.RGBA()
-	// 		s1 := hex.EncodeToString([]byte{byte(v1)})
-
-	// 		s2 := hex.EncodeToString([]byte{byte(v2)})
-	// 		s3 := hex.EncodeToString([]byte{byte(v3)})
-	// 		s4 := hex.EncodeToString([]byte{byte(v4)})
-
-	// 		hexString := fmt.Sprintf("%s%s%s%s", s1, s2, s3, s4)
-	// 		fmt.Printf("%s%s%s%s\n", s1, s2, s3, s4)
-
-	// 		colString := fmt.Sprintf("$color%d = rgba(%s)\n", ind, hexString)
-	// 		colorStrings = append(colorStrings, colString)
-	// 		f2.WriteString(colString)
-	// 		// f2.WriteString(hexString)
-	// 		// f2.WriteString("\n")
-	// 	}
-
-	// }
-
-	// f, _ := os.Create("outColors.png")
-	// png.Encode(f, img)
-
 	genConfig := readConfigTemplate(colorStrings, fileName)
 
-	f3.WriteString(genConfig)
+	// f3.WriteString(genConfig)
+
+	err = writeGeneratedConfigFile(genConfig)
+	if err != nil {
+		log.Printf("[ERROR]: %s", err)
+		os.Exit(1)
+	}
+
+}
+
+func writeGeneratedConfigFile(cfgContent string) error {
+	f3, err := os.Create(CONFIG_FILE_GENERATED)
+	if err != nil {
+		log.Printf("Error generating config file: %s", err)
+		return err
+	}
+
+	_, err = f3.WriteString(cfgContent)
+	if err != nil {
+		log.Printf("Error saving config file: %s", err)
+		return err
+	}
+	log.Printf("[INFO]: Generated config file: %s", f3.Name())
+	return nil
 }
 
 func readConfigTemplate(colors []string, wallPaperPath string) string {
+	log.Printf("[INFO]: Generating config file from template...")
 	data, err := os.Open(CONFIG_FILE_TEMPLATE)
 	if err != nil {
 		log.Printf("Error reading config file template: %s", err)
